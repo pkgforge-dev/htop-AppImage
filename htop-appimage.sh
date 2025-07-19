e5e2927b6ed4d685757c0cf39e919015d5514eb6
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-07-19 02:35:36 -0400

    refactor

--- htop-appimage.sh
+++ htop-appimage.sh
@@ -1,23 +1,22 @@
 #!/bin/sh
 
-set -eu
+set -eux
 
 export ARCH="$(uname -m)"
 export APPIMAGE_EXTRACT_AND_RUN=1
 
 APP=htop
 APPDIR="$APP".AppDir
-SITE="htop-dev/htop"
 UPINFO="gh-releases-zsync|$(echo $GITHUB_REPOSITORY | tr '/' '|')|continuous|*$ARCH.AppImage.zsync"
 APPIMAGETOOL="https://github.com/pkgforge-dev/appimagetool-uruntime/releases/download/continuous/appimagetool-$ARCH.AppImage"
 
 # CREATE DIRECTORIES
-mkdir -p ./"$APP"/AppDir
-cd ./"$APP"/AppDir
+mkdir -p ./AppDir
+cd ./AppDir
 
 # DOWNLOAD AND BUILD HTOP
 CURRENTDIR="$(dirname "$(readlink -f "$0")")" # DO NOT MOVE THIS
-HTOP_URL=$(wget -q https://api.github.com/repos/$SITE/releases -O - \
+HTOP_URL=$(wget -q https://api.github.com/repos/htop-dev/htop/releases -O - \
 	| sed 's/[()",{} ]/\n/g' | grep -oi 'https.*releases.*htop.*tar.xz' | head -1)
 
 wget "$HTOP_URL"
@@ -32,26 +31,19 @@ cd ..
 rm -rf ./htop* ./*.tar.*
 
 # PREPARE APPIMAGE
-cp ./share/applications/*.desktop ./
-cp ./share/pixmaps/* ./htop.png
-ln -s ./htop.png ./.DirIcon
+cp -v ./share/applications/*.desktop ./
+cp -v ./share/pixmaps/*              ./htop.png
+cp -v ./share/pixmaps/*              ./.DirIcon
 
-# AppRun
-cat >> ./AppRun << 'EOF'
-#!/usr/bin/env sh
-CURRENTDIR="$(dirname "$(readlink -f "$0")")"
-"$CURRENTDIR"/bin/htop "$@"
-EOF
-chmod +x ./AppRun
-VERSION="$(./AppRun -V | awk '{print $2}')"
+ln -s ./bin/htop ./AppRun
+chmod +x ./bin/htop
+VERSION="$(./AppRun -V | awk '{print $2; exit}')"
+echo "$VERSION" > ~/version
 
 # MAKE APPIMAGE
 cd ..
-wget -q "$APPIMAGETOOL" -O ./appimagetool
+wget "$APPIMAGETOOL" -O ./appimagetool
 chmod +x ./appimagetool
-./appimagetool --comp zstd \
-	--mksquashfs-opt -Xcompression-level --mksquashfs-opt 22 \
-	-n -u "$UPINFO" "$PWD"/AppDir "$PWD"/"$APP"-"$VERSION"-anylinux-"$ARCH".AppImage
-mv ./*.AppImage* ..
-rm -rf ./"$APP"
+./appimagetool -n -u "$UPINFO" "$PWD"/AppDir "$PWD"/"$APP"-"$VERSION"-anylinux-"$ARCH".AppImage
+
 echo "All Done!"
